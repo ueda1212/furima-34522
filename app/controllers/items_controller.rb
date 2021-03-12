4b8b85df8584dbcd4410732f8e78bdbca83b9156
ueda1212 <78341754+ueda1212@users.noreply.github.com>
2021-03-12 19:35:33 +0900

    商品削除訂正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :not_redirect, only: [:edit, :update]
+  before_action :not_redirect, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
