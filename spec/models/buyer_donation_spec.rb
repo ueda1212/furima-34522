235d34f33203a76843c5da1885837bb83f71143f
ueda1212 <78341754+ueda1212@users.noreply.github.com>
2021-03-18 11:25:46 +0900

    商品購入訂正２追加

--- spec/models/buyer_donation_spec.rb
+++ spec/models/buyer_donation_spec.rb
@@ -89,13 +89,13 @@ RSpec.describe BuyerDonation, type: :model do
             expect(@buyer_donation.errors.full_messages).to include("Phone number is invalid")
           end
 
-          it 'user_idが英数混合だと登録できない' do
+          it 'user_idが空だと登録できない' do
             @buyer_donation.user_id = ""
             @buyer_donation.valid?
             expect(@buyer_donation.errors.full_messages).to include("User can't be blank")
           end
 
-          it 'item_idが英数混合だと登録できない' do
+          it 'item_idが空だと登録できない' do
             @buyer_donation.item_id = ""
             @buyer_donation.valid?
             expect(@buyer_donation.errors.full_messages).to include("Item can't be blank")
